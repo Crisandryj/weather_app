fe172b91d2ae5aee87fa384a780b8e70beef5ff4
crisandryj <crisandryj@gmail.com>
2023-12-06 18:26:37 -0500

    Remove original promise function for weather

--- scripts.js
+++ scripts.js
@@ -1,16 +1,4 @@
-function todaysWeather() {
-  fetch(
-    "http://api.weatherapi.com/v1/current.json?key=40ac864e98b34846a2e172323230512&q=London&aqi=no",
-    { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      console.log(response);
-    });
-}
-async function todaysWeatherAsync() {
+async function todaysWeather() {
   const response = await fetch(
     "http://api.weatherapi.com/v1/current.json?key=40ac864e98b34846a2e172323230512&q=London&aqi=no",
     { mode: "cors" }
@@ -19,4 +7,4 @@ async function todaysWeatherAsync() {
   const data = await console.log(json);
 }
 
-todaysWeatherAsync();
+todaysWeather();
