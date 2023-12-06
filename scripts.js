985fad83a7c107e71b6b37341d34151c56a5234e
crisandryj <crisandryj@gmail.com>
2023-12-06 18:28:37 -0500

    Feat: Add variable to handle apikey and location

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
+const apiKey = "40ac864e98b34846a2e172323230512";
 async function todaysWeather() {
   const response = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=40ac864e98b34846a2e172323230512&q=London&aqi=no",
+    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`,
     { mode: "cors" }
   );
   const json = await response.json();
