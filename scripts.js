fdb925b04f786730449c81ca53f3db98f230b706
crisandryj <crisandryj@gmail.com>
2023-12-07 16:26:02 -0500

    Refactor: create element outside of function

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const apiKey = "40ac864e98b34846a2e172323230512";
 const searchInput = document.querySelector("#search");
 const weather = document.querySelector(".weather");
 const main = document.querySelector("main");
-
+const weatherLocation = document.createElement("div");
 async function todaysWeather(location) {
   const response = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`,
@@ -14,12 +14,14 @@ async function todaysWeather(location) {
 }
 
 function showLocationTemp(data) {
-  const weatherLocation = document.createElement("div");
   weatherLocation.classList.add(".weatherlocal");
   main.insertBefore(weatherLocation, main.childNodes[2]);
   const country = data.location.country;
   weather.textContent = data.current.temp_f;
   weatherLocation.textContent = country;
+  if (country != weatherLocation.textContent) {
+    weatherLocation.textContent = "";
+  }
 }
 
 searchInput.addEventListener("keyup", (e) => {
