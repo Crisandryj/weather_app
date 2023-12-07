08fa06f5df1321395ddf0dddbec4940532479cde
crisandryj <crisandryj@gmail.com>
2023-12-07 15:58:10 -0500

    Feat: Create div and add location

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const apiKey = "40ac864e98b34846a2e172323230512";
 const searchInput = document.querySelector("#search");
 const weather = document.querySelector(".weather");
+const main = document.querySelector("main");
 
 console.log(searchInput);
 console.log(searchInput.value);
@@ -12,7 +13,12 @@ async function todaysWeather(location) {
   );
   const json = await response.json();
   const data = await json;
+  console.log(data);
+  const weatherLocation = document.createElement("div");
+  weatherLocation.classList.add(".weatherlocal");
+  main.appendChild(weatherLocation);
   weather.textContent = data.current.temp_f;
+  weatherLocation.textContent = data.location.country;
 }
 
 searchInput.addEventListener("keyup", (e) => {
