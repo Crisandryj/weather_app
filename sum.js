35db3c7b9aff6dd2f8487785345f65983b39ecef
crisandryj <crisandryj@gmail.com>
2023-12-12 15:28:56 -0500

    Run practice test

--- sum.js
+++ sum.js
@@ -1,4 +1,11 @@
 function sum(a, b) {
   return a + b;
 }
+
+function upCase(string) {
+  const word = string;
+  word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+module.exports = upCase;
 module.exports = sum;
--- sum.test.js
+++ sum.test.js
@@ -1,9 +1,10 @@
 const sum = require("./sum");
+const upCase = require("./sum");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test("capitalize letters", (), ()=>{
-  expect(capitalize("hello")).toBe("Hello")
+test("capitalize letters", () => {
+  expect(upCase("hello")).toBe("Hello");
 });
